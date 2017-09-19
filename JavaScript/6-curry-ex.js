3fc756d42e5fcba76251ab7bc5b04a9fb5bb6709
tshemsedinov <timur.shemsedinov@gmail.com>
2017-09-19 10:11:35 +0300

    Implement curry extended example

---
+++ JavaScript/6-curry-ex.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const sum3 = (a, b, c) => (a + b + c);
+
+const curry = (fn, ...par) => {
+  const curried = (...args) => (
+    fn.length > args.length ? curry(fn.bind(null, ...args)) : fn(...args)
+  );
+  return par.length ? curried(...par) : curried;
+};
+
+const f = curry(sum3);
+const y = f(1)(2)(3);
+console.log(y);
