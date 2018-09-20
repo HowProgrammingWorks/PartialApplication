ba35d95251abe1dcfa439c4ce5fa9a4eda920b4e
tshemsedinov <timur.shemsedinov@gmail.com>
2018-09-20 23:23:22 +0300

    Remove round brackets in single-argument lambdas

--- JavaScript/6-curry.js
+++ JavaScript/6-curry.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const curry = (fn) => (...args) => {
+const curry = fn => (...args) => {
   if (fn.length > args.length) {
     const f = fn.bind(null, ...args);
     return curry(f);
