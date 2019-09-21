a3b50a9d3854cf6dfb40f66c5387993b4b27ef82
Evgeniy Sedov <sedoff-online@yandex.ru>
2019-09-22 01:22:55 +0700

    fix 6-curry-bad.js

--- JavaScript/6-curry-bad.js
+++ JavaScript/6-curry-bad.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const curry = fn => (...args) => (
-  fn.length > args.length ? curry(
+  fn.length > args.length ? (
     ((fn, ...args1) => (...args2) => (
-      fn(...args1.concat(args2))
+      curry(fn)(...args1.concat(args2))
     ))(fn, ...args)
   ) : fn(...args)
 );
@@ -16,9 +16,10 @@ const f = curry(sum4);
 const y1 = sum4(1, 2, 3, 4);
 const y2 = f(1, 2, 3, 4);
 const y3 = f(1, 2, 3)(4);
-//const y4 = f(1, 2)(3)(4);
-//const y5 = f(1)(2)(3)(4);
+const y4 = f(1, 2)(3)(4);
+const y5 = f(1)(2)(3)(4);
 const y6 = f(1)(2, 3, 4);
-//const y7 = f(1)(2)(3, 4);
+const y7 = f(1)(2)(3, 4);
 const y8 = f(1, 2)(3, 4);
 console.log(y1, y2, y3, y6, y8);
+console.log(y4, y5, y7);
