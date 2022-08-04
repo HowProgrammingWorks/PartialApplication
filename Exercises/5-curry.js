275bb5e55084540d1c23e9287a4f8f24f6a2dc70
Orkhan Abbasov <webskyus@gmail.com>
2022-08-04 18:42:59 +0400

    done 1-6 task

--- Exercises/5-curry.js
+++ Exercises/5-curry.js
@@ -2,15 +2,35 @@
 
 // Check 4 digit pin.
 const EXPECTED_PIN = '4967';
-const checkPin = (...code) => code.join('') === EXPECTED_PIN;
+const checkPin = (...code) => {
+  console.log('code', code);
+  return code.join('') === EXPECTED_PIN;
+};
 
 // Define function curry that accepts the length of the function
 // (amount of function arguments) and the function.
+const curry = (length, fn) => {
+  const curried = (...args) => {
+    if (args.length >= length) {
+      return fn.apply(this, args);
+    }
+    return (...newArgs) => curried.apply(this, args.concat(newArgs));
+  };
 
-const curry = (length, fn) => (...args) => null;
+  return curried;
+};
 
-// Allows to enter pin code by one character,
+// Allows entering pin code by one character,
 // Usage: press('3')('4')('5')('6')
 const press = curry(4, checkPin);
 
 module.exports = { press };
+
+
+
+
+
+
+
+
+
--- Exercises/6-chaining.js
+++ Exercises/6-chaining.js
@@ -4,12 +4,21 @@
 const EXPECTED_PIN = '4967';
 const checkPin = (...code) => code.join('') === EXPECTED_PIN;
 
-// Impement function press
+// Implement function press
 // that allows to enter pin code by one character,
 // e.g. press('3').press('4').press('5').press('6')
-//
 // For hint use https://github.com/HowProgrammingWorks/Cheatsheet
 
-const press = null;
+const press = (number) => ({
+  code: [number],
+  press(number) {
+    this.code.push(number);
+    if (this.code.length >= 4) {
+      return checkPin(...this.code);
+    }
+
+    return this;
+  }
+});
 
 module.exports = { press };
