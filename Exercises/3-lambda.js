0e1ababe537b4f099fb1aee8ec88ec28c0f8ae2c
Jhon Mosk <jhon-mosk@yandex.ru>
2023-07-22 15:50:39 +0300

    All tasks completed

--- Exercises/3-lambda.js
+++ Exercises/3-lambda.js
@@ -6,6 +6,10 @@ const tagged = (pref, str) => `[${pref}] ${str}`;
 // E.g. tagDate('My String') === '[2019-11-14] My String'
 // Use function tagged to implement tagDate.
 
-const tagDate = null;
+const tagDate = (str) => {
+  const currentDate = new Date();
+  const formattedDate = currentDate.toISOString().substring(0, 10);
+  return tagged(formattedDate, str);
+};
 
 module.exports = { tagDate };
